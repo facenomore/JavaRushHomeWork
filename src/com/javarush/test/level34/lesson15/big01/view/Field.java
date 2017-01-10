151f828cc9bdcd9f208eda7fc2d1334c3defc9ac
facenomore <afacenomore@gmail.com>
2017-01-10 22:19:23 +0200

    rewrite some code for validator

--- src/com/javarush/test/level34/lesson15/big01/view/Field.java
+++ src/com/javarush/test/level34/lesson15/big01/view/Field.java
@@ -1,6 +1,7 @@
 package com.javarush.test.level34.lesson15.big01.view;
 
 import com.javarush.test.level34.lesson15.big01.controller.EventListener;
+import com.javarush.test.level34.lesson15.big01.model.Direction;
 import com.javarush.test.level34.lesson15.big01.model.GameObject;
 import com.javarush.test.level34.lesson15.big01.model.GameObjects;
 
@@ -8,6 +9,8 @@ import static com.javarush.test.level34.lesson15.big01.model.Model.FIELD_SELL_SI
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Field extends JPanel {
     private View view;
@@ -16,6 +19,9 @@ public class Field extends JPanel {
 
     public Field(View view) {
         this.view = view;
+        KeyHandler keyHandler = new KeyHandler();
+        addKeyListener(keyHandler);
+        setFocusable(true);
     }
 
     public void setEventListener(EventListener eventListener) {
@@ -31,4 +37,32 @@ public class Field extends JPanel {
             gameObject.draw(g);
         }
     }
+
+    public class KeyHandler extends KeyAdapter {
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:  eventListener.move(Direction.LEFT);   break;
+                case KeyEvent.VK_RIGHT: eventListener.move(Direction.RIGHT);  break;
+                case KeyEvent.VK_UP:    eventListener.move(Direction.UP);     break;
+                case KeyEvent.VK_DOWN:  eventListener.move(Direction.DOWN);   break;
+                case KeyEvent.VK_R:     eventListener.restart();              break;
+            }
+        }
+    }
 }
+
+/*
+    Добавим немного интерактивности в нашу игру (перемещение игрока с помощью
+клавиатуры). Начнем с обработки нажатия клавиш клавиатуры.
+14.1.	Добавь в класс Field вложенный класс KeyHandler унаследованный от
+KeyAdapter.
+14.2.	Перегрузи у него метод keyPressed(). Если была нажата клавиша с кодом
+VK_LEFT, то пошли eventListener-у событие move с параметром Direction.LEFT.
+Аналогичным образом обработай нажатия клавиш с кодом VK_RIGHT, VK_UP и
+VK_DOWN. Если пользователь нажал клавишу R с кодом VK_R, то вызови у слушателя
+событий метод restart().
+14.3.	В конструкторе класса Field:
+14.3.1.	Создай объект класса KeyHandler.
+14.3.2.	Установи его слушателем с помощью метода addKeyListener().
+14.3.3.	Установи focusable в true (метод setFocusable()).
+ */
