ea1bb8a8f5baa4fa56badc69f9fb43ac8f8c3079
facenomore <afacenomore@gmail.com>
2017-01-15 14:16:50 +0200

    rewrite some code for validator

---
+++ src/com/javarush/test/level30/lesson04/home01/Consumer.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level30.lesson04.home01;
+
+import java.util.concurrent.TransferQueue;
+
+public class Consumer implements Runnable {
+    public Consumer(TransferQueue<ShareItem> queue) {
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/com/javarush/test/level30/lesson04/home01/Producer.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level30.lesson04.home01;
+
+import java.util.concurrent.TransferQueue;
+
+public class Producer implements Runnable {
+    public Producer(TransferQueue<ShareItem> queue) {
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
