0cb301b5c518905af8d326f05ef61216dc9c58c3
facenomore <afacenomore@gmail.com>
2017-01-16 10:29:46 +0200

    First message

--- src/com/javarush/test/level25/lesson09/task03/Solution.java
+++ src/com/javarush/test/level25/lesson09/task03/Solution.java
@@ -1,5 +1,9 @@
 package com.javarush.test.level25.lesson09.task03;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /* Живем своим умом
 В классе Solution реализуйте интерфейс UncaughtExceptionHandler, который должен:
 1. прервать нить, которая бросила исключение.
@@ -11,13 +15,26 @@ java.lang.RuntimeException: DEF
 java.lang.Exception: ABC
 */
 public class Solution implements Thread.UncaughtExceptionHandler {
+    public static void main(String[] args) {
+        Throwable e = new Exception("ABC", new RuntimeException("DEF", new IllegalAccessException("GHI")));
+        new Solution().uncaughtException(Thread.currentThread(), e);
+
+    }
+
     @Override
     public void uncaughtException(Thread t, Throwable e) {
         t.interrupt();
-        System.out.println(e);
-    }
+        List<String> messages = new ArrayList<>();
+        Throwable tmp = e;
+        if (e != null) messages.add(e.toString());
+        while (tmp.getCause() != null) {
+            tmp = tmp.getCause();
+            messages.add(tmp.toString());
+        }
 
-    public static void main(String[] args) {
-        new Exception("ABC", new RuntimeException("DEF", new IllegalAccessException("GHI")));
+        Collections.reverse(messages);
+        for (String message : messages) System.out.println(message);
+
     }
 }
+
