20be62713187db2b0434231ecf66af4310768161
facenomore <afacenomore@gmail.com>
2017-01-18 23:04:23 +0200

    rewrite some code for validator

---
+++ src/com/javarush/test/level27/lesson09/home01/TransferObject.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level27.lesson09.home01;
+
+public class TransferObject {
+    private int value;
+    protected volatile boolean isValuePresent = false; //use this variable
+
+    public synchronized int get() {
+        System.out.println("Got: " + value);
+        try {
+            while (!isValuePresent) wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+    public synchronized void put(int value) {
+        this.value = value;
+        System.out.println("Put: " + value);
+        notifyAll();
+    }
+}
